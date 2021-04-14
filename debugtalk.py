67597c2d40ae95350f3f9e1081f5d60f8dcb83ca
zhangli920621 <18711307853@163.com>
2021-04-14 20:47:24 +0800

    修改git提交忽略文件

--- debugtalk.py
+++ debugtalk.py
@@ -1,6 +1,7 @@
-import time
-import datetime
 from httprunner import __version__
+import datetime
+import pymysql
+import subprocess
 
 
 def get_httprunner_version():
@@ -15,7 +16,6 @@ def teardown_hook():
     print("后置操作：teardown!")
 
 
-
 # 获取一些时间请求参数格式如‘2021-04-12’
 def get_date(days=None):
     if days is None:
@@ -23,3 +23,16 @@ def get_date(days=None):
     else:
         date = (datetime.datetime.now() + datetime.timedelta(days=days)).strftime("%Y-%m-%d")
     return str(date)
+
+
+# 数据库获取数据
+def sql_res(cmd):
+    conn = pymysql.connect(host="10.6.14.11", port=29966, user="root", password="123456",
+                           database="db_campus_education", charset="utf8")
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute(cmd)
+    res = cursor.fetchall()
+    cursor.close()
+    cursor.close()
+    return res
+
